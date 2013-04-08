6047e0db1bda77a90e60367231ef2616274caf12
Shantanu Das <cnrshantanu@gmail.com>
2013-04-09 01:36:23 +0530

    Fixing notes crashing issue

--- GLWallpaperService/GLWallpaperTest/gen/net/markguerra/android/glwallpapertest/R.java
+++ GLWallpaperService/GLWallpaperTest/gen/net/markguerra/android/glwallpapertest/R.java
@@ -17,18 +17,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int a1=0x7f020000;
-        public static final int android=0x7f020001;
-        public static final int android1=0x7f020002;
-        public static final int android2=0x7f020003;
-        public static final int app_notes=0x7f020004;
-        public static final int ic_menu_compose=0x7f020005;
-        public static final int ic_menu_delete=0x7f020006;
-        public static final int ic_menu_edit=0x7f020007;
-        public static final int ic_menu_revert=0x7f020008;
-        public static final int ic_menu_save=0x7f020009;
-        public static final int icon=0x7f02000a;
-        public static final int live_folder_notes=0x7f02000b;
+        public static final int app_notes=0x7f020000;
+        public static final int ic_menu_compose=0x7f020001;
+        public static final int ic_menu_delete=0x7f020002;
+        public static final int ic_menu_edit=0x7f020003;
+        public static final int ic_menu_revert=0x7f020004;
+        public static final int ic_menu_save=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int live_folder_notes=0x7f020007;
     }
     public static final class id {
         public static final int context_copy=0x7f080008;
